436c362d21d0d0d8eacdb535b20161a150eb7f59
Oy vey <oyiveyi@gmail.com>
2026-03-05 23:01:07 +0300

    Проект Social Media API [#505090]

---
+++ src/main/java/ru/job4j/job4j_social_media_api/Job4jSocialMediaApiApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.job4j_social_media_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jSocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+	}
+}
