dd797afbc0a60fd4b654e837a9a402017cd2384e
Oy vey <oyiveyi@gmail.com>
2026-03-19 00:01:53 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/api/repository/PostRepository.java
+++ src/main/java/ru/job4j/api/repository/PostRepository.java
@@ -1,7 +1,20 @@
 package ru.job4j.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.api.model.Post;
+import ru.job4j.api.model.User;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+import java.time.Instant;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUser(User user);
+
+    List<Post> findByCreatedAtGreaterThanEqualAndCreatedAtLessThanEqual(Instant from, Instant to);
+
+    Page<Post> findAllByOrderByCreatedAtAsc(Pageable pageable);
+
 }
